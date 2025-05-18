bd33295434d121a679f045c3241f99a51333efc1
Arsalan Rahmanian <arsalan@AccountingPrivate2.taksunpars.local>
2025-05-18 18:45:27 +0330

    19

--- src/PersonnelInfo.Core/DTOs/Entities/Employees/AddEmployeeDto.cs
+++ src/PersonnelInfo.Core/DTOs/Entities/Employees/AddEmployeeDto.cs
@@ -3,34 +3,65 @@ using Newtonsoft.Json;
 using PersonnelInfo.Core.DTOs.Validators;
 using System.ComponentModel.DataAnnotations;
 
-public class AddEmployeeDto
+//public class AddAddEmployeeDto
+//{
+//    #region Basic Information
+//    [BindNever]
+//    [Display(Name = "کد پرسنلی")]
+//    public int PersonnelCode { get; set; }
+
+//    [Required]
+//    [StringLength(21, MinimumLength = 3)]
+//    [Display(Name = "نام")]
+//    public string FirstName { get; set; }
+
+//    [Required]
+//    [StringLength(21, MinimumLength = 3)]
+//    [Display(Name = "نام خانوادگی")]
+//    public string LastName { get; set; }
+
+//    [Required]
+//    [NationalId]
+//    [StringLength(10, MinimumLength = 10)]
+//    [RegularExpression(@"^\d+$")]
+//    [Display(Name = "کد ملی")]
+//    public string NationalId { get; set; }
+
+//    [Required]
+//    [StringLength(11, MinimumLength = 11)]
+//    [RegularExpression(@"^\d+$")]
+//    [Display(Name = "شماره همراه")]
+//    public string ContactNumber { get; set; }
+//    #endregion
+//}
+
+
+public class AddAddEmployeeDto
 {
-    #region Basic Information
     [BindNever]
     [Display(Name = "کد پرسنلی")]
     public int PersonnelCode { get; set; }
 
-    [Required]
-    [StringLength(21, MinimumLength = 3)]
+    [Required(ErrorMessage = "AddEmployeeDto.FirstName.Required")]
+    [StringLength(21, MinimumLength = 3, ErrorMessage = "AddEmployeeDto.FirstName.StringLength")]
     [Display(Name = "نام")]
     public string FirstName { get; set; }
 
-    [Required]
-    [StringLength(21, MinimumLength = 3)]
+    [Required(ErrorMessage = "AddEmployeeDto.LastName.Required")]
+    [StringLength(21, MinimumLength = 3, ErrorMessage = "AddEmployeeDto.LastName.StringLength")]
     [Display(Name = "نام خانوادگی")]
     public string LastName { get; set; }
 
-    [Required]
-    [NationalId]
-    [StringLength(10, MinimumLength = 10)]
-    [RegularExpression(@"^\d+$")]
+    [Required(ErrorMessage = "AddEmployeeDto.NationalId.Required")]
+    [NationalId(ErrorMessage = "AddEmployeeDto.NationalId.NationalId")]
+    [StringLength(10, MinimumLength = 10, ErrorMessage = "AddEmployeeDto.NationalId.StringLength")]
+    [RegularExpression(@"^\d+$", ErrorMessage = "AddEmployeeDto.NationalId.RegularExpression")]
     [Display(Name = "کد ملی")]
     public string NationalId { get; set; }
 
-    [Required]
-    [StringLength(11, MinimumLength = 11)]
-    [RegularExpression(@"^\d+$")]
+    [Required(ErrorMessage = "AddEmployeeDto.ContactNumber.Required")]
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "AddEmployeeDto.ContactNumber.StringLength")]
+    [RegularExpression(@"^\d+$", ErrorMessage = "AddEmployeeDto.ContactNumber.RegularExpression")]
     [Display(Name = "شماره همراه")]
     public string ContactNumber { get; set; }
-    #endregion
 }
